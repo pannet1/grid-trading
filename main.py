45ee28ee2b914dea1e00b9996be197f0773e2bc3
uberdeveloper <uberdeveloper001@gmail.com>
2023-09-27 13:02:33 +0530

    paper broker implemented in main

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import numpy as np
 from typing import List, Optional
 from copy import deepcopy
 from strategy import Strategy
-from omspy.brokers.zerodha import Zerodha
-from omspy.simulation.virtual import ReplicaBroker, FakeBroker
 from omspy.order import create_db
 from logzero import logger
 from sqlite_utils import Database
+from broker import paper_broker
+from omspy.brokers.zerodha import Zerodha
 
 # Global variables that would be used throughout the module
-DB = '/tmp/orders.sqlite'
+DB = "/tmp/orders.sqlite"
 
-def get_database()->Optional[Database]:
+
+def get_database() -> Optional[Database]:
     if os.path.exists(DB):
         return Database(DB)
     else:
@@ -28,8 +29,8 @@ def get_database()->Optional[Database]:
 
 def get_all_symbols(strategies: List[Strategy]) -> List[str]:
     """
-    Get the list of all symbols from the given list of 
-    strategies in the format required to extract data 
+    Get the list of all symbols from the given list of
+    strategies in the format required to extract data
     from the broker
     """
     symbols = []
@@ -40,26 +41,31 @@ def get_all_symbols(strategies: List[Strategy]) -> List[str]:
 
 
 def main():
-    parameters = pd.read_csv('parameters.csv').to_dict(orient='records')
+    parameters = pd.read_csv("parameters.csv").to_dict(orient="records")
     strategies = []
-    broker = FakeBroker()
+    broker = paper_broker()
+    connection = get_database()
     for params in parameters:
         try:
             p = {k: v for k, v in params.items() if pd.notnull(v)}
-            strategy = Strategy(**p, broker=broker, datafeed=datafeed)
+            strategy = Strategy(
+                **p, broker=broker, datafeed=datafeed, connection=connection
+            )
             strategies.append(strategy)
         except Exception as e:
             logger.error(e)
     symbols = get_all_symbols(strategies)
-    db = get_database()
-    print(db)
-    print(datafeed.ltp(symbols))
+    # We are mimicking broker here and seeding prices
+    broker.symbols = symbols
+    broker.run()
+    print(connection)
+    print(broker.ltp(symbols))
 
 
 if __name__ == "__main__":
-    config_file = os.path.join(os.environ['HOME'],'systemtrader','config.yaml')
+    config_file = os.path.join(os.environ["HOME"], "systemtrader", "config.yaml")
     with open(config_file) as f:
-        config = yaml.safe_load(f)[0]['config']
+        config = yaml.safe_load(f)[0]["config"]
         datafeed = Zerodha(**config)
         datafeed.authenticate()
     main()
