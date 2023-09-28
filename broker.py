c70e801a850299610ce67e6d5e9f06b98754aa42
uberdeveloper <uberdeveloper001@gmail.com>
2023-09-28 11:32:00 +0530

    add an extra method run2 for fetching quotes from redis

--- broker.py
+++ broker.py
@@ -8,6 +8,16 @@ import os
 import yaml
 from typing import List, Dict, Optional
 from omspy.simulation.models import Instrument
+from omspy.simulation.virtual import generate_price
+
+
+def ltp_from_server(symbols: List[str]) -> Dict[str, float]:
+    """
+    retrieves ltp from redis server for the given list of symbols
+    """
+    # TODO: This method to be implemented
+    return {k: generate_price() for k in symbols}
+    pass
 
 
 def get_actual_broker() -> Zerodha:
@@ -30,7 +40,7 @@ def convert_dict_to_instrument(quotes: Dict[str, Dict]) -> List[Instrument]:
             if type(v) == dict:
                 if all([key in v for key in keys]):
                     inst = Instrument(
-                            name=k[4:], # strip exchange from symbol
+                        name=k[4:],  # strip exchange from symbol
                         token=v["instrument_token"],
                         last_price=v["last_price"],
                         open=v["ohlc"]["open"],
@@ -43,6 +53,25 @@ def convert_dict_to_instrument(quotes: Dict[str, Dict]) -> List[Instrument]:
                     if "oi" in v:
                         inst.open_interest = v["oi"]
                     instruments.append(inst)
+        except Exception as e:
+            logger.error(f"Error {e} in symbol {k}")
+    return instruments
+
+
+def convert_ltp_to_instruments(ltps: Dict[str, float]) -> List[Instrument]:
+    instruments = []
+    for k, v in ltps.items():
+        try:
+            # We are assigning zero to ohlc since they are mandatory
+            inst = Instrument(
+                name=k[4:],  # strip exchange from symbol
+                last_price=v,
+                open=0,
+                high=0,
+                low=0,
+                close=0,
+            )
+            instruments.append(inst)
         except Exception as e:
             logger.error(f"Error {e} in symbol {k}")
     return instruments
@@ -65,6 +94,17 @@ class PaperBroker(ReplicaBroker):
         quotes = self.broker.quote(self.symbols)
         instruments = convert_dict_to_instrument(quotes)
         self.update(instruments)
+        # Mimicks broker order fill based on ltp
+        self.run_fill()
+
+    def run2(self):
+        """
+        Run this method if use ltp from redis server
+        """
+        ltp = ltp_from_server(self.symbols)
+        instruments = convert_ltp_to_instruments(ltp)
+        self.update(instruments)
+        # Mimicks broker order fill based on ltp
         self.run_fill()
 
     def ltp(self, symbols: List[str]) -> Dict[str, float]:
