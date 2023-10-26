b58ca5c5a54f99b3713a8b9942f93d6a7f407b4d
pannet1 <prog@ecomsense.in>
2023-10-26 20:33:56 +0530

    added token searcher

--- grid_trading/search_ws_for_tkns.py
+++ grid_trading/search_ws_for_tkns.py
@@ -1,19 +1,32 @@
-from omspy_brokers.finvasia import Finvasia
-import yaml
-from pprint import pprint
-
-BROKER = Finvasia
-dir_path = "../../"
-with open(dir_path + "config2.yaml", "r") as f:
-    config = yaml.safe_load(f)[0]["config"]
-    print(config)
-    broker = BROKER(**config)
-    if broker.authenticate():
-        print("success")
+from typing import Dict
+import pandas as pd
 
 
-resp = broker.searchscrip(exchange="MCX", searchtext="C")
-pprint(f"{resp=}")
+def get_exchange_token_map_finvasia(
+    key: str = "Token", value: str = "TradingSymbol"
+) -> Dict[int, str]:
+    """
+    Get the exchange token map for finvasia broker
+    key
+        column name to be considered as key
+    value
+        column name to be considered as value
+    Note
+    ----
+    1) Exchange token map generated only for NSE,NFO exchanges
+    2) Token is always in `int` format
+    """
+    df = pd.read_csv("https://shoonya.finvasia.com/MCX_symbols.txt.zip")
+    return {int(k): v for k, v in zip(df[key].values, df[value].values)}
+
+
+search_value = "CRUDEOIL15NOV23P7100"
+resp = get_exchange_token_map_finvasia()
+for k, v in resp.items():
+    if v == search_value:
+        print(f"token  for {search_value} is {k}")
+        break
+
 
 # Add a delay or perform other operations here
 
